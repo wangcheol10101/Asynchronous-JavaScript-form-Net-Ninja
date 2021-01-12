0ef1d1808636f19ed1aeaff2945ef65618169da1
wangcheol10101 <wangcheol10101@gmail.com>
2021-01-12 18:03:56 +0900

    7-promises-commit

--- sandbox.js
+++ sandbox.js
@@ -1,30 +1,56 @@
-const getTodos = (resource, callback) => {
-  const request = new XMLHttpRequest();
-  request.addEventListener('readystatechange', () => {
-    if (request.readyState === 4 && request.status === 200) {
-      //   JSON.parse(): convert JSON string into JS objects
-      const data = JSON.parse(request.responseText);
-      callback(undefined, data);
-    } else if (request.readyState === 4) {
-      callback('could not fetch data', undefined);
-    }
+const getTodos = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4) {
+        reject('error getting resource');
+      }
+    });
+    request.open('GET', resource);
+    request.send();
   });
-
-  // open method
-  request.open('GET', resource);
-  //the request isn't being made, just setting up the request
-
-  // send request
-  request.send();
 };
 
-// get one of data before move on to the next
-getTodos('todos/luigi.json', (err, data) => {
-  console.log(data);
-  getTodos('todos/mario.json', (err, data) => {
-    console.log(data);
-    getTodos('todos/shaun.json', (err, data) => {
-      console.log(data);
-    });
+getTodos('todos/luigi.json')
+  .then((data) => {
+    console.log('promise resolved:', data);
+  })
+  .catch((err) => {
+    console.log('promise rejected:', err);
   });
-});
+
+// promise example
+const getSomething = () => {
+  // to do something in here like get data
+  // when we use promise the first thing we do is return a new promise
+  // Promise is something which is gonna take some time to do
+  // automatically get access to two parameters -- resolve / reject (functions)
+  return new Promise((resolve, reject) => {
+    // do the network request
+    resolve('some data');
+    // reject('some error');
+  });
+};
+
+// fire the first function in then() when resolve the promise
+// the second one fires if we reject
+// the first function takes the data that we passed through to the resolve function
+getSomething().then(
+  (data) => {
+    console.log(data);
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+// a slightly different way we can write
+getSomething()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
