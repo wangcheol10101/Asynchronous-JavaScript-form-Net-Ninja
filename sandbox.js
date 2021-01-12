2d6d2d3f827eb3382eaeb055dc9787876ecd808d
wangcheol10101 <wangcheol10101@gmail.com>
2021-01-12 15:51:33 +0900

    2-HTTP-requests-commit

---
+++ sandbox.js
@@ -0,0 +1,25 @@
+// make a request from js
+// 1. make a request object
+const request = new XMLHttpRequest();
+// XMLHttpRequest() creates a request object (can work with any kind of data)
+// request object is going to be used to send a request from the browser
+
+// event listener and ready-state-change event
+// fires every time there's a state change in the request
+request.addEventListener('readystatechange', () => {
+  // request.readyState: get the state that the current request is in (1, 2, 3, 4)
+  // 상태내용 MDN참조
+  console.log(request, request.readyState);
+  // if the readyState is equal to 4 that's the point when we can do something with the responseText
+  if (request.readyState === 4) {
+    // request.responseTest: contains the response data (a giant string called json data)
+    console.log(request.responseText);
+  }
+});
+
+// open method
+request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+//the request isn't being made, just setting up the request
+
+// send request
+request.send();
