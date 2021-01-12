c48aa8115979bc5783336c8c61b4f191e00e497c
wangcheol10101 <wangcheol10101@gmail.com>
2021-01-12 16:54:51 +0900

    5-using-JSON-data-commit

--- sandbox.js
+++ sandbox.js
@@ -2,24 +2,21 @@ const getTodos = (callback) => {
   const request = new XMLHttpRequest();
   request.addEventListener('readystatechange', () => {
     if (request.readyState === 4 && request.status === 200) {
-      //   console.log(request.responseText);
-      callback(undefined, request.responseText);
+      //   JSON.parse(): convert JSON string into JS objects
+      const data = JSON.parse(request.responseText);
+      callback(undefined, data);
     } else if (request.readyState === 4) {
-      //   console.log('could not fetch the data');
       callback('could not fetch data', undefined);
     }
   });
 
   // open method
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.open('GET', 'todos.json');
   //the request isn't being made, just setting up the request
 
   // send request
   request.send();
 };
-
-console.log(1);
-console.log(2);
 
 getTodos((err, data) => {
   console.log('callback is fired');
@@ -29,6 +26,3 @@ getTodos((err, data) => {
     console.log(data);
   }
 });
-
-console.log(3);
-console.log(4);
