4a0cabf68ed298c8914d2fb63a7e0d082b6d9ca9
wangcheol10101 <wangcheol10101@gmail.com>
2021-01-13 09:19:45 +0900

    8-chaining-promises-commit

--- sandbox.js
+++ sandbox.js
@@ -16,7 +16,15 @@ const getTodos = (resource) => {
 
 getTodos('todos/luigi.json')
   .then((data) => {
-    console.log('promise resolved:', data);
+    console.log('promise 1 resolved:', data);
+    return getTodos('todos/mario.json');
+  })
+  .then((data) => {
+    console.log('promise 2 resolved:', data);
+    return getTodos('todos/shaun.json');
+  })
+  .then((data) => {
+    console.log('promise 3 resolved:', data);
   })
   .catch((err) => {
     console.log('promise rejected:', err);
